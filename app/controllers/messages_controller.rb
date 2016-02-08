2a48ef73b15281789acde9cfd597dc94d4a9b729
cottoncandyybk <claudiayuki.shino@gmail.com>
2016-02-08 13:44:15 +0000

    halt

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,12 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy]
+
   def index
     @message = Message.new
     # Messageを全て取得する。
     @messages = Message.all
   end
-
+  
   def create
     @message = Message.new(message_params)
     if @message.save
@@ -16,8 +18,29 @@ class MessagesController < ApplicationController
       render 'index'
     end
   end
+  
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      redirect_to root_path , notice: 'メッセージを更新しました'
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
+  end
 
   private
+  
+  def set_message
+    @message = Message.find(params[:id])
+  end
+
   def message_params
     params.require(:message).permit(:name, :body)
   end
